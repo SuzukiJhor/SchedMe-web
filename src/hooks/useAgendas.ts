dc1ce052d24281f060dda3f7979f1a653ffbffc9
jhordan.suzuki <jhordan.kendy1@gmail.com>
2025-11-28 20:14:37 -0300

    fix: remove hook agenda

--- src/hooks/useAgendas.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAgendas } from "../services/agendaService";
-
-export function useAgendas() {
-  return useQuery({
-    queryKey: ["agendas"],
-    queryFn: getAgendas,
-  });
-}
